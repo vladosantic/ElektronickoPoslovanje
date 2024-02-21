b9bd7ecf9d1f5440fed3b6c7f4eafd45620007bc
simunjelic <simun.jelic@gmail.com>
2024-02-21 16:35:28 +0100

    Limit to Assistant message length increased to 256 characters and success login pa changed.

--- src/main/java/ba/sum/fsre/elektronickoPoslovanje/security/SpringSecurity.java
+++ src/main/java/ba/sum/fsre/elektronickoPoslovanje/security/SpringSecurity.java
@@ -41,7 +41,7 @@ public class SpringSecurity {
                         form -> form
                                 .loginPage("/login")
                                 .loginProcessingUrl("/login")
-                                .defaultSuccessUrl("/users")
+                                .defaultSuccessUrl("/index")
                                 .permitAll()
                 ).logout(
                         logout -> logout
